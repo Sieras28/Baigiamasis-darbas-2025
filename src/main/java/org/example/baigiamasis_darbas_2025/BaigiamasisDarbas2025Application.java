aa068b57de86060beeb52a5eede05ab8c4653855
Sergej <sergejgorbatenko@yahoo.com>
2025-04-07 18:32:42 +0300

    pom.xml creation

---
+++ src/main/java/org/example/baigiamasis_darbas_2025/BaigiamasisDarbas2025Application.java
@@ -0,0 +1,13 @@
+package org.example.baigiamasis_darbas_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisDarbas2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaigiamasisDarbas2025Application.class, args);
+    }
+
+}
